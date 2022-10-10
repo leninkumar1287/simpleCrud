964b58d1618dccca5f373ce3cc593fcc32bf2fda
leninkumar1287 <leninkumar.vadivel@neosoftmail.com>
2022-10-10 23:44:39 +0530

    Navigation added

---
+++ src/components/Navigation.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navigation() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <Link class="navbar-brand" to="#">Shopping 🛒</Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <Link class="nav-link active" aria-current="page" to="#">Home</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link" to="#">Features</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link" to="#">Pricing</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link disabled" to="#" tabindex="-1" aria-disabled="true">Disabled</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
