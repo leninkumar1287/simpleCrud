92f1ee61cbba16867c7a1e5fd69e40eee720936f
leninkumar1287 <leninkumar.vadivel@neosoftmail.com>
2022-10-11 00:07:22 +0530

    services added

--- src/components/services/ProductServices.js
+++ src/components/services/ProductServices.js
@@ -1,1 +1,21 @@
 import axios from 'axios'
+
+const API = ""
+
+function getProducts() {
+    return axios.get(`${API}`)
+}
+
+function getProductsById(id) {
+    return axios.get(`${API}${id}`)
+}
+
+function updateProductDetails(data) {
+    return axios.put(`${API}`, data)
+}
+
+function deleteProduct(id) {
+    return axios.delete(`${API}${id}`)
+}
+
+export { getProducts, getProductsById, updateProductDetails, deleteProduct }
