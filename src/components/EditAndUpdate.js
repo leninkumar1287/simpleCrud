e01118dd161987fd4b89e164fd2acb8e69a0c6f8
leninkumar_1287 <leninkumar.vadivel@neosoftmail.com>
2022-10-17 10:25:23 +0530

    text box works fine

--- src/components/EditAndUpdate.js
+++ src/components/EditAndUpdate.js
@@ -3,38 +3,40 @@ import { useNavigate } from 'react-router-dom'
 import { getProducts, getProductsById, updateProductDetails } from './services/ProductServices'
 import { useParams } from 'react-router-dom'
 export default function EditAndUpdate() {
-    let {id} = useParams();
-    let [dataPayload, setDataPayload] = useState({})
-    let [viewData, setViewData] = useState({})
+    let { id } = useParams();
+    let [viewData, setViewData] = useState({ productName: '', price: "", quantity: "", productImage: "" })
     const navigation = useNavigate()
-    const dataHanler = (event) => {
-        const { name, value } = event.target
-        setDataPayload({ ...dataPayload, [name]: value })
-    }
 
-    const postData = (event) => {
-        event.preventDefault();
-        updateProductDetails(id,dataPayload).then(res => {
-            console.log(res.data)
-            if (res.data) {
-                alert("New products was added")
-                navigation("/")
-            }
-        })
-    }
     useEffect(() => {
         getProductsById(id).then(response => {
             console.log(" result   : ", response.data)
             setViewData(response.data)
         })
     }, [])
+
+    const dataHanler = (event) => {
+        const { name, value } = event.target
+        setViewData({ ...viewData, [name]: value })
+    }
+
+    const postData = (event) => {
+        event.preventDefault();
+        updateProductDetails(id, viewData).then(res => {
+            console.log(res.data)
+            if (res.data) {
+                alert(" products Details updated")
+                navigation("/")
+            }
+        })
+    }
+
     return (
         <div>
             <h2> Edit product details here</h2>
             <form onSubmit={dataHanler}></form>
             <div className='form-group'>
                 <label > Product Name</label>
-                <input type={"text"} name='productName' className='form-control' value={viewData.productName} onChange={dataHanler}></input>
+                <input type="text" name='productName' className='form-control' value={viewData.productName} onChange={dataHanler}></input>
             </div>
             <div className='form-group'>
                 <label> Price </label>
